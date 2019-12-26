ae41e22f7a55c5b535a4ccba28e06be31596d3b3
Frank Faustino <nawzyah@gmail.com>
2019-12-26 11:12:29 -0800

    ⚡️ Move cron job to child process

---
+++ src/lib/cron.ts
@@ -0,0 +1,25 @@
+import { refreshConversation, refreshAdmins, refreshTeams, refreshUsers } from '../db/intercom'
+
+const { CronJob } = require('cron')
+const exectimer = require('exectimer')
+
+
+const Tick = exectimer.Tick
+let count = 0
+
+// Run cron job every 30 minutes
+new CronJob('*/30 * * * *', async () => {
+  console.log('🚀 cron job start: ', new Date())
+  const tick = new Tick(`cronJob_${count}`)
+  tick.start()
+
+  await refreshUsers()
+  await refreshTeams()
+  await refreshAdmins()
+  await refreshConversation()
+
+  tick.stop()
+  const result = exectimer.timers[`cronJob_${count}`]
+  count++
+  console.log('⏳ cron job time: ', result.parse(result.duration()), '\n✋ cron job count: ', count)
+}).start()
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,21 @@
 import * as dotenv from 'dotenv'
+import * as cp from 'child_process'
 import * as cors from 'cors'
 import * as express from 'express'
 
 dotenv.config({ path: './.env' })
 
-import { job } from './cron'
 import { handleError } from './lib/utils'
+
 
 const { PORT } = process.env
 const app: express.Application = express()
 
-app.use(cors())
-app.use('/api', require('./api'))
-app.use(handleError)
+app
+  .use(cors())
+  .use('/api', require('./api'))
+  .use(handleError)
+  .listen(PORT, () => console.log(`🤖 Server is listening on port ${PORT} in ${app.get('env')} mode`))
 
-// TO-DO: Add worker-farm
-job.start()
-
-app.listen(PORT, () => console.log(`🤖 Server is listening on port ${PORT} in ${app.get('env')} mode`))
+const spawn = cp.fork(require.resolve('./lib/cron.ts'))
+spawn.on('close', code => console.log(`👋 Child process exited with code ${code}`))
